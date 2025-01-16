37d979ac02836cc6e0e3c73e7e6ca2bb0999328f
vitoriarbs <160158820+vitoriarbs@users.noreply.github.com>
2025-01-16 12:21:02 -0300

    doc

--- python-automação/main.py
+++ python-automação/main.py
@@ -2,12 +2,12 @@ import pyautogui
 import pandas as pd
 import time
 
-pyautogui.PAUSE = 3
+pyautogui.PAUSE = 3 #todos os comandos do pyautogui serão executados após 3seg
 
 #Passo 1: Abrir o navegador
-pyautogui.press("win")
-pyautogui.write("chrome")
-pyautogui.press("enter")
+pyautogui.press("win") #press pressiona um botão do teclado
+pyautogui.write("chrome") #write escreve
+pyautogui.press("enter") 
 time.sleep(3)
 
 #Passo 2: Abrir o sistema da empresa (https://dlp.hashtagtreinamentos.com/python/intensivao/login)
@@ -16,34 +16,34 @@ pyautogui.write(link)
 pyautogui.press("enter")
 
 #Passo 2: Fazer login
-time.sleep(3    )
-pyautogui.click(x=705, y=425)
+time.sleep(3)
+pyautogui.click(x=705, y=425) #posição do mause na tela
 pyautogui.write("fulano@gmail.com")
 pyautogui.click(x=690, y=525)
 pyautogui.write("Senha123@")
 pyautogui.click(x=718, y=580)
 
 #Passo 3: Abri a base de dados dos produtos
-tabela = pd.read_csv("produtos.csv")
+tabela = pd.read_csv("produtos.csv") #utilizando pandas para a leitura do arquivo
 print(tabela)
 
 #Passo 4: Cadastrar o 1 produto
 #Passo 5: Repetir o passo 4 até acabar todos os produtos
     
-for linha in tabela.index: #index para pegar as informações das linhas da tabela
+for linha in tabela.index: #index é utilizado para pegar as informações das linhas da tabela
     print(linha)
 
-    # clicar no campo de código
+    # clicar no primeio campo
     pyautogui.click(x=630, y=312)
 
     # pegar da tabela o valor do campo que será preenchido
-    codigo = tabela.loc[linha, "codigo"]
+    codigo = tabela.loc[linha, "codigo"] #loc de localizar
     # preencher o campo
     pyautogui.write(str(codigo))
     # passar para o proximo campo
     pyautogui.press("tab")
+    
     # preencher o campo
-    #loc de localizar
     pyautogui.write(str(tabela.loc[linha, "marca"]))
     pyautogui.press("tab")
     
@@ -68,3 +68,6 @@ for linha in tabela.index: #index para pegar as informações das linhas da tabe
 
     # dar scroll de tudo pra cima
     pyautogui.scroll(1000)
+    
+    #scroll com número positivo pra cima
+    #scroll com número negativo pra baixo    
